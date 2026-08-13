10f21341cefb201ca963a175ecb1a9389efe627e
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 18:41:15 +0300

    ui: add spacing tokens for design system

---
+++ app/src/main/java/eu/kanade/presentation/components/ui/Spacing.kt
@@ -0,0 +1,11 @@
+package eu.kanade.presentation.components.ui
+
+import androidx.compose.ui.unit.dp
+
+object AppSpacing {
+    val extraSmall = 2.dp
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 16.dp
+    val extraLarge = 24.dp
+}

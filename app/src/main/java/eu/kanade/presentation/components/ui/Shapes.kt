cd5ab0f1ed3e96e440909135097a7464037395b2
johnmuriuki774-source <johnmuriuki774@gmail.com>
2026-08-13 18:42:24 +0300

    ui: add shape tokens for design system

---
+++ app/src/main/java/eu/kanade/presentation/components/ui/Shapes.kt
@@ -0,0 +1,10 @@
+package eu.kanade.presentation.components.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object AppShapes {
+    val small = RoundedCornerShape(6.dp)
+    val medium = RoundedCornerShape(10.dp)
+    val large = RoundedCornerShape(16.dp)
+}
